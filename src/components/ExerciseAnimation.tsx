020bc5a26619d0add90bec6d5abc95198fd1fb93
kck0920 <kck0920@gmail.com>
2026-07-22 21:19:23 +0900

    feat: apply Giphy GIF for Mountain Climber exercise animation

--- src/components/ExerciseAnimation.tsx
+++ src/components/ExerciseAnimation.tsx
@@ -7,6 +7,7 @@ const GIF_MAP: Record<string, string> = {
   jumpingJacks: 'https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExYnF4cHBpcGs4ZDZxd3I3cGo3YnRwem9vaGNvZmwydHZzZTc0Z2NoaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/WgViBJUWQMEeu5Jvmd/giphy.gif',
   squat: 'https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExejBkNzZ1ZG4wMGFocnByaWlkMWN4OXBoano5ZHgxZjE5eXZodnI1biZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/fYHUeuuuFa3VFBOLzA/giphy.gif',
   pushup: 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZjkwNWlmNDBuMjc3eHR6ZHMyczd1OWUybHRvOHluZ29yMmhqMjk5OCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/1JdJzYK1SEZVaCgj1P/giphy.gif',
+  mountainClimber: 'https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3NnN4c3ZxdzgwdWJ4cXM5OWI5aW1qaWx3aHFldWVsaWlra3h3dHVpYSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/vI2BMBcFDgbbFrB0bA/giphy.gif',
 }
 
 export function ExerciseAnimation({ exerciseName = '', isExercise }: ExerciseAnimationProps) {
